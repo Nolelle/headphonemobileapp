67c7f68d150ccca52c24ea221ad269d4610791f1
IdkHow2 <benjamin.simms@edu.sait.ca>
2024-11-28 12:31:01 -0700

    finished the 'Presets Page'
    along with finishing the Presets Page, i added
    - Settings Page
    - Sound Test Page
    - Preset 1 and 2 navigation buttons
       > SnackBar notification to indicate its
           selected
    
    have fun Baljot!

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,352 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MainPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MainPageState createState() => _MainPageState();
 }
+class _MainPageState extends State<MainPage> {
+  int _currentIndex = 1; //starting on the 'Presets' page
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  //the pages
+  final List<Widget> _pages = [
+    const SoundTestPage(),//this will be empty
+    const PresetsPage(),
+    const SettingsPage(),//this will be empty
+  ];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final List<String> _titles = [
+    'Sound Test',
+    'Presets',
+    'Settings',
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        backgroundColor: const Color.fromRGBO(133, 86, 169, 1.00),
+        title: Align(
+          alignment: Alignment.center,
+          child: Text(
+            _titles[_currentIndex], // Dynamic title based on the current page
+            style: const TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+        ),
+      ),
+      backgroundColor: const Color.fromRGBO(237, 212, 254, 1.00),
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: const Color.fromRGBO(133, 86, 169, 1.00),
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white70,
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.volume_up), label: 'Sound Test'),
+          BottomNavigationBarItem(icon: Icon(Icons.equalizer_rounded), label: 'Presets'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
+        ],
+      ),
+    );
+  }
+}
+
+class PresetsPage extends StatefulWidget {
+  const PresetsPage({super.key});
+
+  @override
+  _PresetsPageState createState() => _PresetsPageState();
+}
+class _PresetsPageState extends State<PresetsPage> {
+  int selected_button = 0; //0 is for preset 1, and 1 is for preset 2
+
+  final Color selected_button_color = const Color.fromRGBO(93, 59, 129, 1.00);
+  final Color unselected_button_color = const Color.fromRGBO(133, 86, 169, 1.00);
+
+  @override
+  Widget build(BuildContext context) {
+    //temp headphones attributes
+    const String HEADPHONE_NAME = "John's Headphones";
+    const int BATTERY_LEVEL = 73;
+    const bool IS_CONNECTED = true;
+
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(237, 212, 254, 1.00),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //headphone name
+            const Text(
+                HEADPHONE_NAME,
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                )
+            ),
+            const SizedBox(height: 16),
+            //battery level
+            const Text(
+              'Battery: $BATTERY_LEVEL%',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.black54,
+              ),
+            ),
+            const SizedBox(height: 8),
+            //connection status
+            const Text(
+              'Connection Status:',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.black54,
+              ),
+            ),
+            const Text(
+              IS_CONNECTED ? "Connected" : "Not Connected",
+              style: TextStyle(
+                fontSize: 20,
+                color: IS_CONNECTED ? Colors.green : Colors.red,
+              ),
+            ),
+            //preset 1
+            const SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  selected_button = 0; // Select Preset 1
+                });
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('\'Preset 1\' Successfully Sent To Device!'),
+                    duration: Duration(seconds: 1),
+                  ),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor:
+                    selected_button == 0 ? selected_button_color : unselected_button_color,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                minimumSize: const Size(double.infinity, 50),
+              ),
+              child: Align(
+                alignment: Alignment.center,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        'Preset 1',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => const Preset2Page()),
+                          );
+                        },
+                        child: const Icon(
+                          Icons.edit,
+                          color: Colors.white,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 8),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  selected_button = 1; // Select Preset 2
+                });
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('\'Preset 2\' Successfully Sent To Device!'),
+                    duration: Duration(seconds: 1),
+                  ),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor:
+                    selected_button == 1 ? selected_button_color : unselected_button_color,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                minimumSize: const Size(double.infinity, 50),
+              ),
+              child: Align(
+                alignment: Alignment.center,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        'Preset 2',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => const Preset2Page()),
+                          );
+                        },
+                        child: const Icon(
+                          Icons.edit,
+                          color: Colors.white,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+//BALJOT WORK HERE
+//don't actually work on all the pages, just do the 'Preset 1' page and duplicate it to 'Preset 2' page
+class SoundTestPage extends StatelessWidget {
+  const SoundTestPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child:
+      Text(
+        '¯\\_(ツ)_/¯',
+        style: TextStyle(
+          fontSize: 25,
+          fontWeight: FontWeight.bold,
+          color: Color.fromRGBO(133, 86, 169, 1.00),
+        ),
+      ),
+    );
+  }
+}
+class Preset1Page extends StatelessWidget {//will not have the bottom bar, only back button
+  const Preset1Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromRGBO(133, 86, 169, 1.00),
+        title: const Align(
+          alignment: Alignment.center,
+          child: Text(
+            'Preset 1',
+            style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios_new_rounded, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context); // Custom behavior for back button
+          },
+        ),
+      ),
+      backgroundColor: const Color.fromRGBO(237, 212, 254, 1.00),
+      body: const Text('Nothing is here!'),
+    );
+  }
+}
+class Preset2Page extends StatelessWidget {//will not have the bottom bar, only back button
+  const Preset2Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromRGBO(133, 86, 169, 1.00),
+        title: const Align(
+          alignment: Alignment.center,
+          child: Text(
+            'Preset 2',
+            style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios_new_rounded, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context); // Custom behavior for back button
+          },
+        ),
+      ),
+      backgroundColor: const Color.fromRGBO(237, 212, 254, 1.00),
+      body: const Text('Nothing is here!'),
+    );
+  }
+}
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child:
+        Text(
+          '¯\\_(ツ)_/¯',
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+            color: Color.fromRGBO(133, 86, 169, 1.00),
+          ),
+        ),
     );
   }
 }
