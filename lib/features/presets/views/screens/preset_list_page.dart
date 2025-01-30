1a591431ab5dddbd12cdaa0ab18268d525da9a20
IdkHow2 <benjamin.simms@edu.sait.ca>
2025-01-30 15:21:45 -0700

    testing audio
    so far, we got the audio to play, and its Eminem's "I'm Back", this is only temporary as we try to see what can be done with it

--- lib/features/presets/views/screens/preset_list_page.dart
+++ lib/features/presets/views/screens/preset_list_page.dart
@@ -5,7 +5,7 @@ import './preset_page.dart';
 import '../../providers/preset_provider.dart';
 import '../../models/preset.dart';
 
-class PresetsListPage extends StatelessWidget {
+class PresetsListPage extends StatefulWidget {
   final PresetProvider presetProvider;
 
   const PresetsListPage({
@@ -13,8 +13,6 @@ class PresetsListPage extends StatelessWidget {
     required this.presetProvider,
   });
 
-<<<<<<< Updated upstream
-=======
   @override
   State<PresetsListPage> createState() => _PresetsListPageState();
 }
@@ -22,43 +20,58 @@ class PresetsListPage extends StatelessWidget {
 class _PresetsListPageState extends State<PresetsListPage> {
   String? activePresetId; // Tracks the currently active preset ID
   final player = AudioPlayer();
-<<<<<<< Updated upstream
-=======
 
   Future<void> playSound() async {
     String audioPath = "audio/eminem.mp3";
     await player.play(AssetSource(audioPath));
   }
->>>>>>> Stashed changes
 
-  Future<void> playSound() async {
-    String audioPath = "audio/eminem.mp3";
-    await player.play(AssetSource(audioPath)  );
-  }
-
->>>>>>> Stashed changes
   Future<bool> _showConfirmationDialog(
       BuildContext context, String presetName) async {
     return await showDialog<bool>(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: const Text('Confirm Preset Activation'),
-              content:
-                  Text('Do you want to send "$presetName" to your device?'),
-              actions: <Widget>[
-                TextButton(
-                  child: const Text('Cancel'),
-                  onPressed: () => Navigator.of(context).pop(false),
-                ),
-                TextButton(
-                  child: const Text('Send'),
-                  onPressed: () => Navigator.of(context).pop(true),
-                ),
-              ],
-            );
-          },
-        ) ??
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Confirm Preset Activation'),
+          content:
+          Text('Do you want to send "$presetName" to your device?'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () => Navigator.of(context).pop(false),
+            ),
+            TextButton(
+              child: const Text('Send'),
+              onPressed: () => Navigator.of(context).pop(true),
+            ),
+          ],
+        );
+      },
+    ) ??
+        false;
+  }
+
+  Future<bool> _showDeleteConfirmationDialog(
+      BuildContext context, String presetName) async {
+    return await showDialog<bool>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Confirm Delete'),
+          content: Text('Are you sure you want to delete "$presetName"?'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () => Navigator.of(context).pop(false),
+            ),
+            TextButton(
+              child: const Text('Delete'),
+              onPressed: () => Navigator.of(context).pop(true),
+            ),
+          ],
+        );
+      },
+    ) ??
         false;
   }
 
@@ -66,82 +79,135 @@ class _PresetsListPageState extends State<PresetsListPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromRGBO(237, 212, 254, 1.00),
-      body: Consumer<PresetProvider>(
-        builder: (context, provider, child) {
-          final presets = provider.presets.values.toList();
-          return ListView.builder(
-            padding: const EdgeInsets.all(16.0),
-            itemCount: presets.length,
-            itemBuilder: (context, index) {
-              final preset = presets[index];
-              final isActive = preset.id == provider.activePresetId;
+      body: Column(
+        children: [
+          Expanded(
+            child: Consumer<PresetProvider>(
+              builder: (context, provider, child) {
+                final presets = provider.presets.values.toList();
+                return ListView.builder(
+                  padding: const EdgeInsets.all(16.0),
+                  itemCount: presets.length,
+                  itemBuilder: (context, index) {
+                    final preset = presets[index];
+                    final isActive = preset.id == provider.activePresetId;
 
-              return Padding(
-                padding: const EdgeInsets.only(bottom: 8.0),
-                child: ElevatedButton(
-                  onPressed: () async {
-                    final shouldActivate =
-                        await _showConfirmationDialog(context, preset.name);
-
-                    if (shouldActivate) {
-                      provider.setActivePreset(preset.id);
-                      if (context.mounted) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content: Text(
-                                '\'${preset.name}\' Successfully Sent To Device!'),
-                            duration: const Duration(seconds: 1),
-                          ),
-                        );
-                      }
-                    }
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: isActive
-                        ? const Color.fromRGBO(93, 59, 129, 1.00)
-                        : const Color.fromRGBO(133, 86, 169, 1.00),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    minimumSize: const Size(double.infinity, 50),
-                  ),
-                  child: Align(
-                    alignment: Alignment.center,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    return Padding(
+                      padding: const EdgeInsets.only(bottom: 8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text(
-                            preset.name,
-                            style: const TextStyle(
-                              fontSize: 20,
-                              color: Colors.white,
+                          ElevatedButton(
+                            onPressed: () async {
+                              final shouldActivate = await _showConfirmationDialog(
+                                  context, preset.name);
+
+                              if (shouldActivate) {
+                                setState(() {
+                                  activePresetId =
+                                      preset.id; // Show dropdown for this preset
+                                });
+
+                                provider.setActivePreset(preset.id);
+
+                                if (context.mounted) {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      content: Text(
+                                          '${preset.name} Successfully Sent To Device!'),
+                                      duration: const Duration(seconds: 1),
+                                    ),
+                                  );
+                                }
+                              }
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: isActive
+                                  ? const Color.fromRGBO(93, 59, 129, 1.00)
+                                  : const Color.fromRGBO(133, 86, 169, 1.00),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              minimumSize: const Size(double.infinity, 50),
+                            ),
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Padding(
+                                padding:
+                                const EdgeInsets.symmetric(horizontal: 8),
+                                child: Row(
+                                  mainAxisAlignment:
+                                  MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      preset.name,
+                                      style: const TextStyle(
+                                        fontSize: 20,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                    const Icon(
+                                      Icons.arrow_forward,
+                                      color: Colors.white,
+                                    ),
+                                  ],
+                                ),
+                              ),
                             ),
                           ),
-                          GestureDetector(
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => PresetPage(
-                                    presetId: preset.id,
-                                    presetName: preset.name,
-                                    presetProvider: provider,
-                                  ),
+                          // Show dropdown only if this preset is active
+                          if (activePresetId == preset.id)
+                            DropdownButton<String>(
+                              value: null, // No initial value for the dropdown
+                              hint: const Text('What does thou want to do :3'),
+                              onChanged: (String? value) async {
+                                if (value == null) return;
+
+                                switch (value) {
+                                  case 'edit':
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => PresetPage(
+                                          presetId: preset.id,
+                                          presetName: preset.name,
+                                          presetProvider: widget.presetProvider,
+                                        ),
+                                      ),
+                                    );
+                                    break;
+                                  case 'delete':
+                                    final shouldDelete = await _showDeleteConfirmationDialog(
+                                        context, preset.name);
+                                    if (shouldDelete) {
+                                      provider.deletePreset(preset.id);
+                                      setState(() {
+                                        activePresetId = null; // Hide dropdown
+                                      });
+                                      ScaffoldMessenger.of(context)
+                                          .showSnackBar(
+                                        SnackBar(
+                                          content: Text(
+                                              '${preset.name} deleted successfully!'),
+                                        ),
+                                      );
+                                    }
+                                    break;
+                                }
+                              },
+                              items: const [
+                                DropdownMenuItem(
+                                  value: 'edit',
+                                  child: Text('Edit'),
                                 ),
-                              );
-                            },
-                            child: const Icon(
-                              Icons.edit,
-                              color: Colors.white,
+                                DropdownMenuItem(
+                                  value: 'delete',
+                                  child: Text('Delete'),
+                                ),
+                              ],
                             ),
-                          )
                         ],
                       ),
-<<<<<<< Updated upstream
-                    ),
-=======
                     );
                   },
                 );
@@ -170,23 +236,27 @@ class _PresetsListPageState extends State<PresetsListPage> {
                     fontSize: 18,
                     color: Colors.black,
                     fontWeight: FontWeight.bold,
->>>>>>> Stashed changes
                   ),
                 ),
               );
             },
-<<<<<<< Updated upstream
-          );
-        },
-=======
           ),
 
         ],
->>>>>>> Stashed changes
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: const Color.fromRGBO(133, 86, 169, 1.00),
         onPressed: () async {
+          final presetCount = widget.presetProvider.presets.length;
+          if (presetCount >= 10) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('You can only have a maximum of 10 presets!'),
+              ),
+            );
+            return;
+          }
+
           final newId = 'preset_${DateTime.now().millisecondsSinceEpoch}';
           final newPreset = Preset(
             id: newId,
@@ -203,7 +273,7 @@ class _PresetsListPageState extends State<PresetsListPage> {
             },
           );
 
-          await presetProvider.createPreset(newPreset);
+          await widget.presetProvider.createPreset(newPreset);
 
           if (context.mounted) {
             Navigator.push(
@@ -212,7 +282,7 @@ class _PresetsListPageState extends State<PresetsListPage> {
                 builder: (context) => PresetPage(
                   presetId: newId,
                   presetName: 'New Preset',
-                  presetProvider: presetProvider,
+                  presetProvider: widget.presetProvider,
                 ),
               ),
             );
